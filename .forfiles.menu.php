1a7214f313b876dd6076bbf89fd084488adcc41c
itxor <poltishru@gmail.com>
2017-02-06 10:23:05 +0700

    next commit

---
+++ .forfiles.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+    Array(
+        "О продукте",
+        "http://www.gczn.nsk.su/index.php",
+        Array(),
+        Array(),
+        ""
+    )
+    /*Array(
+        "Информация",
+        "/feedback/law-map/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "English",
+        "/english/",
+        Array(),
+        Array(),
+        ""
+    )*/
+);
+?>
